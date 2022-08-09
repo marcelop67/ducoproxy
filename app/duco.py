919c5a0870b94e05a7f7f58b0da3c1218b68a46e
Marcel Opsteegh <marcel.opsteegh@gmail.com>
2022-08-09 23:39:05 +0200

    Changed error handling

--- app/duco.py
+++ app/duco.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 import urllib.parse
 import mysql.connector
@@ -6,15 +7,18 @@ import json
 import config
 
 CONFIG = config.Configuration()
+LOGGER = logging.getLogger('duco')
+
 DUCO_ROOT_ENDPOINT = str(urllib.parse.urlunparse(CONFIG.url('DUCO_ROOT_ENDPOINT')))
 MYSQL_HOST = CONFIG.str('MYSQL_HOST')
 MYSQL_PORT = CONFIG.int('MYSQL_PORT', 3306)
-MYSQL_DB = CONFIG.str('MYSQL_DB', 'ducologger')
+MYSQL_DB = CONFIG.str('MYSQL_DB')
 MYSQL_USER = CONFIG.str('MYSQL_USER')
 MYSQL_PASSWORD = CONFIG.str('MYSQL_PASSWORD')
 
 def getinfo():
     info = {
+        'timestamp': datetime.now(tz=timezone.utc),
         'box': {
             'logtime': None,
             'mode': None, 
@@ -37,170 +41,134 @@ def getinfo():
 
     session = requests.Session()
     session.headers.update({'Accept': 'application/json', 'Content-Type': 'application/json'})
-    logtime = datetime.now(tz=timezone.utc)
     
-    try:
-        url = f"{DUCO_ROOT_ENDPOINT}/board_info"
-        response = session.get(url, timeout=10)
-        response.raise_for_status()
-        board = response.json()
-        info['box']['uptime'] = board['uptime']
+    url = f"{DUCO_ROOT_ENDPOINT}/board_info"
+    response = session.get(url, timeout=10)
+    response.raise_for_status()
+    board = response.json()
+    info['box']['uptime'] = board['uptime']
+    
+    url = f"{DUCO_ROOT_ENDPOINT}/nodelist')"
+    response = session.get(url, timeout=10)
+    response.raise_for_status()     
+    nodelist = response.json()['nodelist']
+
+    location = None
+    target = 0  
+    for n in nodelist:
+        url = f"{DUCO_ROOT_ENDPOINT}/nodeinfoget"
+        response = session.get(url, params={'node': n}, timeout=10)
+        response.raise_for_status()         
+        node = response.json()
+
+        if node['devtype'] == 'BOX':
+            info['box']['mode'] = node['mode']
+            info['box']['state'] = node['state']
+            info['box']['level'] = node['trgt']
+            info['box']['cntdwn'] = node['cntdwn']
+                
+            url = f"{DUCO_ROOT_ENDPOINT}/boxinfoget')"
+            response = session.get(url, timeout=10)
+            response.raise_for_status()     
+            box = response.json()
+
+            info['box']['temp_oda'] = box['EnergyInfo']['TempODA']/10 # Outdoor air. Supply air from outdoors to the unit
+            info['box']['temp_sup'] = box['EnergyInfo']['TempSUP']/10 # Supply air. Supply air from unit to house
+            info['box']['temp_eta'] = box['EnergyInfo']['TempETA']/10 # Extract air. Supply air from the house to the unit
+            info['box']['temp_eha'] = box['EnergyInfo']['TempEHA']/10 # Exhaust air. Exhaust air from the unit to outdoors        
+            info['box']['remainfilter_days'] = box['EnergyInfo']['FilterRemainingTime']
+            info['box']['remainfilter_percent'] = round(box['EnergyInfo']['FilterRemainingTime']/(365/2)*100)
+            info['box']['bypass'] = box['EnergyInfo']['BypassStatus']
+            info['box']['frost'] = box['EnergyInfo']['FrostProtState']
+            info['box']['power'] = (box['EnergyFan']['SupplyFanPwmLevel'] + box['EnergyFan']['ExhaustFanPwmLevel'])/1000
         
-        url = f"{DUCO_ROOT_ENDPOINT}/nodelist')"
-        response = session.get(url, timeout=10)
-        response.raise_for_status()     
-        nodelist = response.json()['nodelist']
+        elif node['devtype'] == 'UCCO2' or node['devtype'] == 'UCRH' or (node['devtype'] == 'UC' and node['netw'] == 'VIRT'):
+            sensor = {}
+            sensor['nid'] = n
+            sensor['devtype'] = node['devtype']
+            sensor['location'] = node['location']
+            sensor['target'] = node['snsr']
+            sensor['temp'] = None
+            sensor['co2'] = None
+            sensor['rh'] = None
 
-        location = None
-        target = 0  
-        for n in nodelist:
-            url = f"{DUCO_ROOT_ENDPOINT}/nodeinfoget"
-            response = session.get(url, params={'node': n}, timeout=10)
-            response.raise_for_status()         
-            node = response.json()
+            if node['devtype'] == 'UCCO2':
+                sensor['temp'] = node['temp']
+                sensor['co2'] = node['co2']
+            elif node['devtype'] == 'UCRH':
+                sensor['temp'] = node['temp']
+                sensor['rh'] = node['rh']
+            else: # (node['devtype'] == 'UC' and node['netw'] == 'VIRT')
+                sensor['devtype'] = 'PROGRM'
+                sensor['location'] = 'Program'
 
-            if node['devtype'] == 'BOX':
-                info['box']['logtime'] = logtime
-                info['box']['mode'] = node['mode']
-                info['box']['state'] = node['state']
-                info['box']['level'] = node['trgt']
-                info['box']['cntdwn'] = node['cntdwn']
-                  
-                url = f"{DUCO_ROOT_ENDPOINT}/boxinfoget')"
-                response = session.get(url, timeout=10)
-                response.raise_for_status()     
-                box = response.json()
+            info['sensors'].append(sensor)
+            if sensor['target'] > target:
+                location = sensor['location']
+                target = sensor['target']
 
-                info['box']['temp_oda'] = box['EnergyInfo']['TempODA']/10 # Outdoor air. Supply air from outdoors to the unit
-                info['box']['temp_sup'] = box['EnergyInfo']['TempSUP']/10 # Supply air. Supply air from unit to house
-                info['box']['temp_eta'] = box['EnergyInfo']['TempETA']/10 # Extract air. Supply air from the house to the unit
-                info['box']['temp_eha'] = box['EnergyInfo']['TempEHA']/10 # Exhaust air. Exhaust air from the unit to outdoors        
-                info['box']['remainfilter_days'] = box['EnergyInfo']['FilterRemainingTime']
-                info['box']['remainfilter_percent'] = round(box['EnergyInfo']['FilterRemainingTime']/(365/2)*100)
-                info['box']['bypass'] = box['EnergyInfo']['BypassStatus']
-                info['box']['frost'] = box['EnergyInfo']['FrostProtState']
-                info['box']['power'] = (box['EnergyFan']['SupplyFanPwmLevel'] + box['EnergyFan']['ExhaustFanPwmLevel'])/1000
-            
-            elif node['devtype'] == 'UCCO2' or node['devtype'] == 'UCRH' or (node['devtype'] == 'UC' and node['netw'] == 'VIRT'):
-                sensor = {}
-                sensor['logtime'] = logtime
-                sensor['nid'] = n
-                sensor['devtype'] = node['devtype']
-                sensor['location'] = node['location']
-                sensor['target'] = node['snsr']
-                sensor['temp'] = None
-                sensor['co2'] = None
-                sensor['rh'] = None
+    info['box']['has_control'] = 'Manual' if info['box']['mode'] == 'MANU' else location
 
-                if node['devtype'] == 'UCCO2':
-                    sensor['temp'] = node['temp']
-                    sensor['co2'] = node['co2']
-                elif node['devtype'] == 'UCRH':
-                    sensor['temp'] = node['temp']
-                    sensor['rh'] = node['rh']
-                else: # (node['devtype'] == 'UC' and node['netw'] == 'VIRT')
-                    sensor['devtype'] = 'PROGRM'
-                    sensor['location'] = 'Program'
+    session.close()
 
-                info['sensors'].append(sensor)
-                if sensor['target'] > target:
-                    location = sensor['location']
-                    target = sensor['target']
-
-        info['box']['has_control'] = 'Manual' if info['box']['mode'] == 'MANU' else location
-    except Exception as err: 
-        info = {'error': {}}
-        status_code = requests.codes.internal_server_error
-        info['error']['code'] = status_code
-        info['error']['reason'] = err.__class__.__name__
-        info['error']['message'] = str(err)
-    else:
-        status_code = requests.codes.ok
-    finally:
-        session.close()
- 
-    return info, status_code
+    return info
 
 def writelog():
-    info, status_code = getinfo()
-    if status_code != requests.codes.ok:
-        return info, status_code
-
+    info = getinfo()
+ 
     result = {
-        'timestamp': info['box']['logtime'],
+        'timestamp': info['timestamp'],
         'box_logid': None,
         'sensor_logids': []
     }
 
-    try:
-        db = mysql.connector.connect(
-            host=MYSQL_HOST,
-            port=MYSQL_PORT,
-            db=MYSQL_DB,
-            user=MYSQL_USER,
-            password=MYSQL_PASSWORD
-        )
-        c = db.cursor()
-        
-        box = info['box']
-        sql = f"INSERT INTO box ({', '.join(box.keys())}) VALUES ({', '.join(['%s'] * len(box.values()))})"
-        c.execute(sql, list(box.values()))
-        result['box_logid'] = c.lastrowid
+    db = mysql.connector.connect(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        db=MYSQL_DB,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD
+    )
+    c = db.cursor()
+    
+    box = info['box']
+    box['logtime'] = info['timestamp']
+    sql = f"INSERT INTO box ({', '.join(box.keys())}) VALUES ({', '.join(['%s'] * len(box.values()))})"
+    c.execute(sql, list(box.values()))
+    result['box_logid'] = c.lastrowid
 
-        for sensor in info['sensors']:
-            sensor['box_logid'] = result['box_logid']
-            sql = f"INSERT INTO sensors ({', '.join(sensor.keys())}) VALUES ({', '.join(['%s'] * len(sensor.values()))})"
-            c.execute(sql, list(sensor.values()))
-            result['sensor_logids'].append(c.lastrowid)
+    for sensor in info['sensors']:
+        sensor['logtime'] = info['timestamp']
+        sensor['box_logid'] = result['box_logid']
+        sql = f"INSERT INTO sensors ({', '.join(sensor.keys())}) VALUES ({', '.join(['%s'] * len(sensor.values()))})"
+        c.execute(sql, list(sensor.values()))
+        result['sensor_logids'].append(c.lastrowid)
 
-        db.commit()
-        db.close()
-    except Exception as err: 
-        result = {'error': {}}
-        status_code = requests.codes.internal_server_error
-        result['error']['code'] = status_code
-        result['error']['reason'] = err.__class__.__name__
-        result['error']['message'] = str(err)
-    else:
-        status_code = requests.codes.ok
- 
-    return result, status_code
+    db.commit()
+    db.close()
+
+    return result
 
 def chksensor(location, field, threshold):
-    info, status_code = getinfo()
-    if status_code != requests.codes.ok:
-        return info, status_code
+    info = getinfo()
 
-    try:
-        sensor = {}
-        for n in range(len(info['sensors'])):
-            if info['sensors'][n]['location'].casefold() == location.casefold():
-                sensor = info['sensors'][n]
-    
-        result = {
-            'field': field,
-            'value': sensor[field],
-            'threshold': threshold,
-            'ok': float(sensor[field]) <= float(threshold)
-        }
-    except Exception as err: 
-        result = {'error': {}}
-        status_code = requests.codes.bad_request
-        result['error']['code'] = status_code
-        result['error']['reason'] = err.__class__.__name__
-        result['error']['message'] = str(err)
-    else:
-        status_code = requests.codes.ok
+    sensor = {}
+    for n in range(len(info['sensors'])):
+        if info['sensors'][n]['location'].casefold() == location.casefold():
+            sensor = info['sensors'][n]
+
+    result = {
+        'timestamp': info['timestamp'],
+        'field': field,
+        'value': sensor[field],
+        'threshold': threshold,
+        'ok': float(sensor[field]) <= float(threshold)
+    }
  
-    return result, status_code
+    return result
 
 if __name__ == "__main__":
     # For debugging while developing
-    info, status_code = getinfo()
-    print(status_code)
-    print(json.dumps(info, indent=4, default=str))
-
-    stat, status_code = chksensor('Keuken', 'co2', 500)
-    print(status_code)
-    print(json.dumps(stat, indent=4, default=str))
-
+    # print(json.dumps(getinfo(), indent=4, default=str))
+    # print(json.dumps(chksensor('Keuken', 'co2', 500), indent=4, default=str))
+    print(json.dumps(writelog(), indent=4, default=str))
--- app/main.py
+++ app/main.py
@@ -1,21 +1,44 @@
-from flask import Flask
-import requests
+from flask import Flask, abort
+import logging
 import duco
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/')
 def main():
-    return duco.getinfo()
+    try:
+        info = duco.getinfo()
+    except Exception as err:
+        app.logger.exception('An exception occured')
+        return abort(500)
+    else:
+        return info
 
 @app.route('/log')
 def log():
-    return duco.writelog()
+    try:
+        result = duco.writelog()
+    except Exception as err:
+        app.logger.exception('An exception occured')
+        return abort(500)
+    else:
+        return result
+
 
 @app.route('/check/<string:location>/<string:field>/<float:threshold>')
 def check(location, field, threshold):
-    return duco.chksensor(location, field, threshold)
+    try:
+        result = duco.chksensor(location, field, threshold)
+    except KeyError as err:
+        return abort(404)
+    except Exception as err:
+        app.logger.exception('An exception occured')
+        return abort(500)
+    else:
+        return result
  
 if __name__ == '__main__':
     # Only for debugging while developing
