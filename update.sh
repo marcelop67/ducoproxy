c2ad322a80004a5fea9f641211e245fe70aceffe
Marcel Opsteegh <marcel.opsteegh@gmail.com>
2022-08-11 11:36:07 +0200

    Renamed .yml to .yaml

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-IMAGE=$(grep -oP "image: \K.+" docker-compose.yml)
+IMAGE=$(grep -oP "image: \K.+" docker-compose.yaml)
 docker pull $IMAGE
 docker-compose stop
 docker-compose rm --force
